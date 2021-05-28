82c4188919fbf927fa505b7acf858a60f15c4b24
Joo-Yeon-Kim <rlawndus555@naver.com>
2021-05-28 23:40:15 +0900

    update django

--- jooyeon/likelion_edu/likelion_session/django/firstproject/firstapp/views.py
+++ jooyeon/likelion_edu/likelion_session/django/firstproject/firstapp/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 
-def welcom(request):
+def welcome(request):
     return render(request, "welcome.html")
+
+
+def hello(request):
+    userName = request.GET['name']
+    return render(request, 'hello.html', {'userName': userName})
 
