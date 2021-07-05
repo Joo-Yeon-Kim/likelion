9f29e0340ca810dc790f9b7705c50343fcd653df
Joo-Yeon-Kim <rlawndus555@naver.com>
2021-07-05 12:54:06 +0900

    django4

--- jooyeon/likelion_edu/likelion_session/django2/lionproject/lionproject/settings.py
+++ jooyeon/likelion_edu/likelion_session/django2/lionproject/lionproject/settings.py
@@ -142,7 +142,7 @@ AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = 'likelion-django-jooyeon'
 AWS_S3_SIGNATURE_VERSION = 's3v4'
 AWS_S3_REGION_NAME = 'ap-northeast-2'
-#AWS_S3_CUSTOM_DOMAIN = ''
+AWS_S3_CUSTOM_DOMAIN = 'd37gx43o3lnt1n.cloudfront.net'
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
